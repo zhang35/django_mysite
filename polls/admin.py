170fad6ccb93e97390470abd349b9789f3daff1b
Jinsane#5 <zhangjqfriend@126.com>
2021-06-15 23:44:11 +0800

    done

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice  # Choice objects are edited on the Question admin page
+    extra = 3  # provide enough fields for 3 choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        ('Text', {'fields': ['question_text']}),
+        ('Date information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -12,6 +13,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
