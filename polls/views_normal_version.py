79abc005051d155e3c0f1a94fae773c6f496f317
zhang35 <zhangjqfriend@126.com>
2021-06-15 18:02:24 +0800

    part5 finished

--- polls/views_normal_version.py
+++ polls/views_normal_version.py
@@ -2,35 +2,26 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.utils import timezone
-from django.views import generic
+
 from polls.models import Question, Choice
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+def detail(request, question_id):
+    quesiton = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': quesiton})
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -50,5 +41,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
 
-        # reverse() 用来通过name来定位url，return a string like '/polls/3/results/'
+        # reverse() return a string like '/polls/3/results/'
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
